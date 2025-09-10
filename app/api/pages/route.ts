b7f12d7cc9ae9a480c4f49b2163b318fecd15d47
LazTech <laznology.dev@gmail.com>
2025-09-10 15:39:54 +0700

    add: POST endpoint for create new page

--- app/api/pages/route.ts
+++ app/api/pages/route.ts
@@ -1,5 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import authOptions from "@/lib/auth";
 
 export async function GET() {
   try {
@@ -14,4 +16,27 @@ export async function GET() {
   } catch {
     return NextResponse.error();
   }
+}
+
+export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user || !session.user.id) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+  }
+
+  const { title } = await request.json();
+
+  const slug = title
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-");
+  await prisma.page.create({
+    data: {
+      title,
+      slug,
+      authorId: session.user.id,
+    },
+  });
+  return NextResponse.json({ success: true }, { status: 201 });
 }
