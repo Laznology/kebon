397159f50f52b539599000ed0d7149b58d2834c7
LazTech <laznology.dev@gmail.com>
2025-09-10 15:35:03 +0700

    ehc: add callback jwt and session; extends next auth session and jwt interface

--- lib/auth.ts
+++ lib/auth.ts
@@ -50,6 +50,23 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/signin",
   },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        const id = token?.id || token.sub;
+        if (id) {
+          session.user.id = id as string;
+        }
+      }
+      return session;
+    },
+  },
 };
 
 export async function auth() {
