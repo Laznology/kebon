3964510758841637bf9778f429f24875477cce04
LazTech <laznology.dev@gmail.com>
2025-09-10 20:34:17 +0700

    force autofocus for search input

--- components/search-modal.tsx
+++ components/search-modal.tsx
@@ -115,6 +115,7 @@ export default function SearchModal({
             </ActionIcon>
           }
           autoFocus
+          data-autofocus
           size="md"
         />
         {isSearching && (
